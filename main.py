2583b20590fcdaf5128094bf509d1c664c77f5bc
kishan <kkishan1000@gmail.com>
2023-03-02 19:20:38 +0530

    added 2 functions main_menu and book_menu

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 
 import mysql.connector
 from mysql.connector import errorcode
+import os
+
+from utils import my_input, my_output
 
 from books import Books
 from suppliers import Suppliers
@@ -13,6 +16,110 @@ HOST = 'localhost'
 USER = 'Kishan'  # user name here
 PASS = 'kishan'  # password
 DATABASE = 'management'
+
+def book_menu():
+    os.system('cls')
+    msg = '''
+    *************************************************
+                    BOOK MENU
+    *************************************************
+        1. ADD
+        2. UPDATE PRICE
+        3. SEARCH
+        4. UPDATE STATUS
+        5. DISPLAY ALL
+        6. RETURN TO MAIN MENU
+    '''
+    my_output(msg)
+    c = my_input(int, 'Enter Your Choice : ')
+
+    if c == 1:
+        books.add()
+        input()
+    elif c == 2:
+        books.update_price()
+        input()
+    elif c == 3:
+        books.search()
+        input()
+    elif c == 4:
+        books.update()
+        input()
+    elif c == 5:
+        books.display()
+        input()
+    elif c == 6:
+        main_menu()
+    else:
+        print('Wrong input')
+        input()
+    
+    book_menu()
+
+def sup_menu():
+    pass
+
+def pur_menu():
+    pass
+
+def emp_menu():
+    pass
+
+def mem_menu():
+    pass
+
+def sal_menu():
+    pass
+
+def main_menu():
+    os.system('cls')
+    msg = f'''
+    *************************************************
+                BOOKSHOP MANGEMENT SYSTEM
+    *************************************************
+        1. BOOKS
+        2. SUPPLIERS
+        3. PURCHASES
+        4. EMPLOYEES
+        5. MEMBERS
+        6. SALES
+        7. EXIT
+    '''
+
+    my_output(msg)
+    c = my_input(int, 'Enter Your Choice : ')
+
+    if c == 1:
+        os.system('cls')
+        book_menu()
+        input()
+    elif c == 2:
+        os.system('cls')
+        sup_menu()
+        input()
+    elif c == 3:
+        os.system('cls')
+        pur_menu()
+        input()
+    elif c == 4:
+        os.system('cls')
+        emp_menu()
+        input()
+    elif c == 5:
+        os.system('cls')
+        mem_menu()
+        input()
+    elif c == 6:
+        os.system('cls')
+        sal_menu()
+        input()
+    elif c == 7:
+        exit(1)
+    else:
+        print('Wrong input')
+        main_menu()
+    
+    return
 
 
 if __name__ == '__main__':
@@ -77,4 +184,6 @@ if __name__ == '__main__':
     # books.display()
     # members.diaplay()
     # sales.add_bill()
-    sales.find_total_sales()
+    # sales.find_total_sales()
+
+    main_menu()
